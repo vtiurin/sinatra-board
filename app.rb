1d238f07b9cda096d16173d7e78d7147d85261e3
Vladimir Tiurin <bfg.tyurin@gmail.com>
2018-09-19 22:29:42 +0300

    add Comments table and view

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ def init_db
   @db.results_as_hash = true
 end
 
-before '/' do
+before do
   init_db
 end
 
@@ -19,6 +19,13 @@ configure do
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created_date DATE,
     content TEXT
+  )'
+  @db.execute 'create table if not exists Comments
+  (
+    id integer primary key autoincrement,
+    created_date date,
+    content text,
+    post_id integer
   )'
   @db.close
 end
@@ -40,6 +47,35 @@ post '/new' do
     return erb :new
   end
   @db.execute "INSERT INTO Posts (created_date, content) VALUES (datetime('now', 'localtime'), ?)", [post_content]
-  @bdb.close
-  erb post_content
+  @db.close
+  redirect to '/'
+end
+
+get '/details/:post_id' do
+  post_id = params[:post_id]
+  posts = @db.execute 'select * from Posts where id=?', [post_id]
+  @post = posts[0]
+  @comments =  @db.execute 'select * from Comments where post_id=? order by created_date', [@post['id']]
+  @db.close
+  erb :details
+end
+
+post '/details/:post_id' do
+  content = params[:content]
+  post_id = params[:post_id]
+  @db.execute "insert into Comments
+    (
+      created_date,
+      content,
+      post_id
+    )
+      values
+    (
+      datetime('now',
+      'localtime'),
+      ?,
+      ?
+    )", [content, post_id]
+  @db.close
+  redirect to "/details/#{post_id}"
 end
