0bac6bbe4f64105c91bdeadb418d179072d6582c
Vladimir Tiurin <bfg.tyurin@gmail.com>
2018-09-19 09:10:44 +0300

    init database, make board.db

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
-require "sinatra/reloader" if development?
+require 'sinatra/reloader' if development?
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'board.db'
+  @db.results_as_hash = true
+end
+
+before '/new' do
+  init_db
+end
 
 get '/' do
   erb 'hi'
